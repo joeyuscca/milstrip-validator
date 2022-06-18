6a328bf9571bb10b6eec580da61454121035fe0c
Joey Bayes <joey.bayes@uscca.com>
2022-06-17 23:40:28 -0400

    Add tests for invalid length.

--- src/validator/validator.spec.ts
+++ src/validator/validator.spec.ts
@@ -2,6 +2,7 @@ import { IValidator, Validator } from './validator';
 
 describe('Validator', () => {
   let classUnderTest: IValidator;
+  const invalidMilstrip = '________________________________________________________________________________'
 
   beforeEach(() => {
     classUnderTest = new Validator();
@@ -9,5 +10,20 @@ describe('Validator', () => {
 
   it('should construct', () => {
     expect(classUnderTest).toBeDefined();
+  });
+
+  it('should not return errors including "Invalid Length" when given a milstrip that is 80 characters', () => {
+    const errors = classUnderTest.Validate(invalidMilstrip);
+    expect(errors).not.toContain('Invalid Length');
+  });
+
+  it('should return errors including "Invalid Length" when given a milstrip less than 80 characters', () => {
+    const errors = classUnderTest.Validate(invalidMilstrip.slice(0, 78));
+    expect(errors).toContain('Invalid Length');
+  });
+
+  it('should return errors including "Invalid Length" when given a milstrip more than 80 characters', () => {
+    const errors = classUnderTest.Validate(`${invalidMilstrip}_`);
+    expect(errors).toContain('Invalid Length');
   });
 });
